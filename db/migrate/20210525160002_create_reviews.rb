35e2f9ca2cb1065d8ff3e1ab094e2233141fdbd9
Your Name <you@example.com>
2021-05-25 13:30:47 -0600

    added belongs to and has many

--- db/migrate/20210525160002_create_reviews.rb
+++ db/migrate/20210525160002_create_reviews.rb
@@ -2,6 +2,8 @@ class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
       t.string :content
+      t.integer :user_id
+      t.integer :game_id
 
       t.timestamps
     end
