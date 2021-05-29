262c5ced15566fcbcb449bae8faa864c30894f9a
Your Name <you@example.com>
2021-05-28 18:14:35 -0600

    added index and new method to reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,23 @@
 class ReviewsController < ApplicationController
+    before_action :redirect_if_not_logged_in
 
     def index
+        if params[:game_id] && @game = Game.find_by_id(params[:game_id])
+            @reviews = @game.reviews
+        else
+            @reviews = Review.all
+        end
     end
 
-    def show 
+    def show
     end
 
     def new
+        if params[:game_id] && @game = Game.find_by_id(params[:game_id]) && !current_user
+            @review = @game.reviews.build
+        else
+            @review = Review.new
+        end
     end
 
     def create
@@ -20,6 +31,10 @@ class ReviewsController < ApplicationController
 
     def destroy
     end
+
+    private
+
+    
     
 end
 
