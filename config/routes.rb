077897655e6a16c6a63ea05ef0c7f32eeb388a75
Cameron <Camnn3@yahoo.com>
2021-06-09 14:53:26 -0600

    created routes and nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
+  root "sessions#home"
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+
+  delete '/logout' => 'sessions#destroy'
+
+  resources :games do
+    resources :reviews
+  end
+  resources :users do
+    resources :games, only: [:new,:create,:index,:show]
+  end
+  resources :reviews 
+
+  get "/auth/:provider/callback" => 'sessions#omniauth'
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  
 end
