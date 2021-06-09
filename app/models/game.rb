afa26b20f27c2cb8ec356c9034a675e39eca3c45
Cameron <Camnn3@yahoo.com>
2021-06-09 14:46:13 -0600

    redid my game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,5 @@
 class Game < ApplicationRecord
+    has_many :review_users, through: :reviews , source: :users 
     has_many :reviews
-    has_many :users, through: :reviews
+    #has_many :users, through: :reviews
 end
