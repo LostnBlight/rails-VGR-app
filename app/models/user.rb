7d51384db4c5770255663d79212fa72547b04c70
Cameron <Camnn3@yahoo.com>
2021-06-09 14:46:31 -0600

    re did my user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :games, through :reviews
+    #has_many :games
+    #has_many :games, through: :reviews
+    has_many :reviewed_games, through: :reviews, source: :game
 
     has_secure_password
 end
