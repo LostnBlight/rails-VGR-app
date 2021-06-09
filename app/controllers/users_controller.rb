37c001f8f19e4070440d3bfee8d094d5a6f42b33
Cameron <Camnn3@yahoo.com>
2021-06-09 14:45:19 -0600

    updated amy create and update for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,30 @@
 class UsersController < ApplicationController
+    require 'pry'
 
     def show 
+        redirect_if_not_logged_in
         @user = User.find_by_id(params[:id])
     end
 
     def new
         @user = User.new
+        binding.pry
     end
 
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:successful] = "Welcome #{@user.name} to Video Game Reviews"
+            flash[:successful] = "Welcome #{@user.username} to Video Game Reviews"
             session[:user_id] = @user.id
-            redirect_to user_path(@user)
+            redirect_to games_path
         else
-            render: new
+            render :new
         end
+    end
+
+    def update
+        @user = User.find_by_id(session[:user_id])
+        redirect_to '/' if !@user
     end
     
     private
