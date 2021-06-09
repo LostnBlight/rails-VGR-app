cd4fb2ffbc99aea0f7a314d5e93972d2a81e4132
Cameron <Camnn3@yahoo.com>
2021-06-09 14:43:37 -0600

    I added a create, update and destroy for games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,24 +1,57 @@
 class GamesController < ApplicationController
+    require 'pry'
+    def display
+        @user = User.find(params[:id])
+        if @user.id = session[:user_id]
+        @games = @user.reviewed_games    
+        else
+            redirect_to(home_path)     
+        end
+    end
 
     def index
+       @games = Game.all.order(:name)
     end
 
     def show 
+        @game = Game.find_by_id(params[:id])
     end
 
     def new
+        @game = Game.new
     end
 
     def create
+        @game = Game.new(game_params)
+        if @game.save
+            redirect_to  games_path
+        else
+            render :new
+        end
     end
     
     def edit
     end
 
     def update
+        find_game
+            if find_game.update(game_params)
+                redirect_to games_path
+            else    
+                render :edit  
+            end
     end
 
     def destroy
+        if find_game.destroy
+            redirect_to games_path
+        end 
+    end
+
+    private
+
+    def game_params
+        params.require(:game).permit(:name)
     end
     
 end
