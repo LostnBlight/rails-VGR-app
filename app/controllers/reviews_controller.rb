681433333a53f8308a4e30966a09e0d6d5df62e9
Cameron <Camnn3@yahoo.com>
2021-06-09 14:44:12 -0600

    added my html routes and create, edit and destroy for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,38 +1,62 @@
 class ReviewsController < ApplicationController
     before_action :redirect_if_not_logged_in
-
-    def index
-        if params[:game_id] && @game = Game.find_by_id(params[:game_id])
-            @reviews = @game.reviews
-        else
-            @reviews = Review.all
-        end
-    end
+    require 'pry'
 
     def show
+        find_review
+        @game = @review.game
     end
 
     def new
-        if params[:game_id] && @game = Game.find_by_id(params[:game_id]) && !current_user
-            @review = @game.reviews.build
-        else
-            @review = Review.new
-        end
+        @game = Game.find_by_id(params[:game_id])
+        @review = @game.reviews.new
     end
 
     def create
+        find_game
+            @review = find_game.reviews.new(review_params)
+            @review.user = current_user
+            if @review.save
+            redirect_to game_path(@review.game)
+            else
+                render :new 
+            end
     end
     
     def edit
+        find_review
+        @game = @review.game
     end
 
     def update
+        find_review
+        if @review.update(review_params)
+            redirect_to game_path(@review.game)
+        else    
+            render :edit   
+        end
     end
 
     def destroy
+        find_review
+            if find_review.destroy
+                redirect_to '/'
+            end 
     end
 
     private
+
+    def find_game
+        @game = Game.find(params[:game_id])
+    end
+
+    def find_review
+        @review = Review.find(params[:id])
+    end 
+
+    def review_params
+        params.require(:review).permit(:content, :user_id, :game_id)
+    end
 
     
     
